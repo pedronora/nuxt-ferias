f765544fdbeb6a689f05f08f96bb5ff0ee4effe1
Pedro Nora <pedro@nora.vc>
2025-10-29 11:36:11 -0300

    Stop tracking .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/pedro/Documentos/Projetos/nuxt-ferias/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/pedro/Documentos/Projetos/nuxt-ferias/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
